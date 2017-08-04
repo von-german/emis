2bc840545a4104b695f9bb072f59b9552a238c20
Will von Behren <willvb3@hotmail.com>
2017-08-04 00:38:07 -0500

    Add protection against editing different users and implement forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,11 +8,24 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def current_user?(user)
+      user == current_user
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def redirect_back_or(default)
+      redirect_to(session[:forwarding_url] || default)
+      session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
